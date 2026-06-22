4fbb9e558401f60c65ca3bd2dbcb99dfb221de32
Nuskan Nazar <nuskanm988@gmail.com>
2026-06-22 17:33:29 +0530

    Integrate skills section into homepage layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Skills from "../components/Skills";
 
 function Home() {
   return (
@@ -8,6 +9,7 @@ function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Skills />
     </>
   );
 }
