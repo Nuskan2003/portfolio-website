dbd3fb670f49d09799113afc3ea94dc33e86795f
Nuskan Nazar <nuskanm988@gmail.com>
2026-06-22 17:39:00 +0530

    Configure routing for project case study pages

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
+import CaseStudy from "../pages/CaseStudy";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route
+        path="/case-study/:slug"
+        element={<CaseStudy />}
+      />
     </Routes>
   );
 }
