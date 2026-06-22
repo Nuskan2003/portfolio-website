482294cc8414db71b47f144ed43b4bd5fb8fcc6d
Nuskan Nazar <nuskanm988@gmail.com>
2026-06-22 17:39:35 +0530

    Integrate experience timeline and projects section into homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Skills from "../components/Skills";
+import Experience from "../components/Experience";
+import Projects from "../components/Projects";
 
 function Home() {
   return (
@@ -10,6 +12,8 @@ function Home() {
       <Hero />
       <About />
       <Skills />
+      <Experience />
+      <Projects />
     </>
   );
 }
