2d21201a4c12ad8d8c78b0c8385a5599ae15c2ba
Nuskan Nazar <nuskanm988@gmail.com>
2026-06-22 17:51:20 +0530

    Integrate animated statistics section into homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import About from "../components/About";
+import Stats from "../components/Stats";
 import Skills from "../components/Skills";
 import Experience from "../components/Experience";
 import Projects from "../components/Projects";
@@ -16,6 +17,8 @@ function Home() {
       <Hero />
 
       <About />
+
+      <Stats />
 
       <Skills />
 
