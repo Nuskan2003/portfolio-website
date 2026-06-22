8a7174fb37ed8f5bae38b3c0fd9ef2cbb4b66118
Nuskan Nazar <nuskanm988@gmail.com>
2026-06-22 17:46:54 +0530

    Integrate contact footer and scroll to top components into homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,16 +4,30 @@ import About from "../components/About";
 import Skills from "../components/Skills";
 import Experience from "../components/Experience";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
+import Footer from "../components/Footer";
+import ScrollToTopButton from "../components/ScrollToTopButton";
 
 function Home() {
   return (
     <>
       <Navbar />
+
       <Hero />
+
       <About />
+
       <Skills />
+
       <Experience />
+
       <Projects />
+
+      <Contact />
+
+      <Footer />
+
+      <ScrollToTopButton />
     </>
   );
 }
